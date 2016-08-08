455f15e5984814d2f688c26a44a330b0a7fadd8f
Robert Katzki <katzki@ubilabs.net>
2016-08-08 16:36:42 +0200

    review: remove files for review

--- test/lib/helpers.js
+++
@@ -1,8 +0,0 @@
-import sinon from 'sinon';
-
-export default {
-  getMockEndpoint(result = 'result', error = null) {
-    return options => // eslint-disable-line no-unused-vars
-      sinon.stub().callsArgWith(1, error, result);
-  }
-};
